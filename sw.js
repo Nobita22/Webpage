476ead74daccd8a4108c3a58e4fc561760cc5b76
Nobita <58627889+Nobita22@users.noreply.github.com>
2025-08-22 10:08:07 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+
+  const btn = document.getElementById('redirectBtn');
+  btn.textContent = 'Install App';
+  btn.addEventListener('click', () => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
 
 
 importScripts(
